c18a88aa9c9ac41b9b47b4dabe315f7d8ac22fa5
Yash-raj-singh2804 <yashrajsingh284006@gmail.com>
2026-01-02 19:03:27 +0530

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -1,6 +1,5 @@
 from passlib.context import CryptContext
 
-# Use Argon2 instead of bcrypt
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def hash_password(password: str) -> str:
@@ -8,3 +7,4 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
